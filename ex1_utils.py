6786ffc485dd96e26ec31af71266234de1974c85
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2022-03-24 09:06:05 +0200

    Add files via upload

---
+++ ex1_utils.py
@@ -0,0 +1,81 @@
+"""
+        '########:'##::::'##::::'##:::
+         ##.....::. ##::'##:::'####:::
+         ##::::::::. ##'##::::.. ##:::
+         ######:::::. ###::::::: ##:::
+         ##...:::::: ## ##:::::: ##:::
+         ##:::::::: ##:. ##::::: ##:::
+         ########: ##:::. ##::'######:
+        ........::..:::::..:::......::
+"""
+from typing import List
+
+import numpy as np
+LOAD_GRAY_SCALE = 1
+LOAD_RGB = 2
+
+
+def myID() -> np.int:
+    """
+    Return my ID (not the friend's ID I copied from)
+    :return: int
+    """
+    return 123456789
+
+
+def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
+    """
+    Reads an image, and returns the image converted as requested
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: The image object
+    """
+    pass
+
+
+def imDisplay(filename: str, representation: int):
+    """
+    Reads an image as RGB or GRAY_SCALE and displays it
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: None
+    """
+    pass
+
+
+def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
+    """
+    Converts an RGB image to YIQ color space
+    :param imgRGB: An Image in RGB
+    :return: A YIQ in image color space
+    """
+    pass
+
+
+def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
+    """
+    Converts an YIQ image to RGB color space
+    :param imgYIQ: An Image in YIQ
+    :return: A RGB in image color space
+    """
+    pass
+
+
+def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+        Equalizes the histogram of an image
+        :param imgOrig: Original Histogram
+        :ret
+    """
+    pass
+
+
+def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+    """
+        Quantized an image in to **nQuant** colors
+        :param imOrig: The original image (RGB or Gray scale)
+        :param nQuant: Number of colors to quantize the image to
+        :param nIter: Number of optimization loops
+        :return: (List[qImage_i],List[error_i])
+    """
+    pass
