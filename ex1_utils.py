c875b15ed69319a0acf5e8c6f96d04be3a70e694
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2022-03-30 23:42:34 +0300

    All functions finished. Comments have been written.

--- ex1_utils.py
+++ ex1_utils.py
@@ -12,10 +12,6 @@ from typing import List
 import numpy as np
 import cv2  # library of Python bindings designed to solve computer vision problems.
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import normalize, MinMaxScaler
-from skimage import io
-from sklearn.cluster import KMeans
-#from sklearn.utils import
 import math   #to import math module
 
 LOAD_GRAY_SCALE = 1
@@ -37,48 +33,19 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
-
-#     img_file_path = filename
-#
-#     if representation == 1:  # If representation is grayscale
-#     # Using cv2.cvtColor() method
-#     # Using cv2.COLOR_BGR2GRAY color space
-#     # conversion code
-#         src = cv2.imread(img_file_path)  # Create the numpy array of the image
-#         img_array = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-#         normed_matrix = normlized(img_array,1) #normlize
-#         return normed_matrix
-#
-#     elif representation == 2:  # If representation is RGB
-#         img_array = cv2.imread(img_file_path)#Create the numpy array of the image.
-#         img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
-#         normed_matrix = normlized(img_array,2) #normlize
-#         return normed_matrix
-#
-# def normlized (img:np.ndarray ,representation: int)->np.ndarray:
-#     """
-#
-#     :param img:
-#     :param representation:
-#     :return:
-#     """
-#     min_val=img.min()
-#     max_val=img.max()
-#     normlized_arr=(img- float(min_val)) /float(max_val-min_val)
-#     return normlized_arr
     if representation != 1 and representation != 2:
             raise ValueError('only 1 or 2 are possible inputs for representation, ''to output Grayscale or RGB pics respectively')
     filepath = filename
-    if representation == 1:
+    if representation == 1: # if grayScale
         channel = 0
-    elif representation == 2:
+    elif representation == 2: # if RGB
         channel = 1
     img = cv2.imread(filepath, channel)
-    if img is None:
-        raise ValueError("Could not find requested file inside 'pics' folder. ")
+    if img is None: # raised an error
+        raise ValueError("Could not find requested file inside folder. ")
     if channel:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.normalize(img.astype('double'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    img = cv2.normalize(img.astype('double'), None, 0.0, 1.0, cv2.NORM_MINMAX) # normlized values to 0-1
     return img
 
 
@@ -108,25 +75,13 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
 
     constant_mat = [[0.299, 0.587, 0.114],
                     [0.596, -0.275, -0.321],
-                    [0.212, -0.523, 0.311]]#define the constant mat with constant values
+                    [0.212, -0.523, 0.311]] # define the constant mat with constant values
     Ans_mat = np.zeros_like(imgRGB.astype(float)) # create new np.array of zeroes
 
     shape = imgRGB.shape[0]
-    for val in range(shape):#move over all pixels in image
+    for val in range(shape): # move over all pixels in image
         Ans_mat[val, ...] = np.matmul(imgRGB[val, ...], constant_mat)
     return Ans_mat
-
-    # yiq_ = np.array([[0.299, 0.587, 0.114],
-    #                  [0.596, -0.275, -0.321],
-    #                  [0.212, -0.523, 0.311]])
-    # imYI = np.dot(imgRGB, yiq_.T.copy())
-    # return imYI
-    # mat = np.array([[0.299, 0.587, 0.144], [0.596, -0.275, -0.321], [0.212, -0.523, 0.311]])
-    # for x in imgRGB:
-    #     for y in x:
-    #         y[:] = mat.dot(y[:])
-    # return imgRGB
-
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     """
@@ -134,26 +89,15 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
     """
-
-    constant_mat = [[1, 0.956, 0.619], [1, -0.272, -0.647], [1, -1.106, 1.703]]#define the constant mat with constant values
+    constant_mat = [[1, 0.956, 0.619],
+                    [1, -0.272, -0.647],
+                    [1, -1.106, 1.703]] # define the constant mat with constant values
     Ans_mat = np.zeros_like(imgYIQ.astype(float)) # create new np.array of zeroes
 
     shape = imgYIQ.shape[0]
-    for val in range(shape):#move over all pixels in image
+    for val in range(shape): # move over all pixels in image
         Ans_mat[val, ...] = np.matmul(imgYIQ[val, ...], constant_mat)
     return Ans_mat
-
-    # mat = np.array([[1, 0.956, 0.619], [1, -0.272, -0.647], [1, -1.106, 1.703]])
-    # for x in imgYIQ:
-    #     for y in x:
-    #         y[:] = mat.dot(y[:])
-    # return imgYIQ
-
-    # rgb_ = np.array([[1.00, 0.956, 0.623],
-    #                 [1.0, -0.272, -0.648],
-    #                 [1.0, -1.105, 0.705]])
-    # imRGB = np.dot(imgYIQ, rgb_.T.copy())
-    # return imRGB
 
 def Regular_histogram_Building(imgOrig: np.ndarray) -> (np.ndarray,int):
     """
@@ -182,14 +126,11 @@ def Regular_histogram_Building(imgOrig: np.ndarray) -> (np.ndarray,int):
         YIQimage = transformRGB2YIQ(imgOrig)  # convert the RGB array to YIQ
         Ychannel = YIQimage[:, :, 0]  # we will extract the first column as Y channel (those are normalized values between 0-1)
         # we want to turn the normalized values to values between 0-255,
-        # therefore first we will multiply every element in 255
-    # scaler_define = MinMaxScaler(feature_range=(0, 255))
-    # scaledYchannel = scaler_define.fit_transform(Ychannel)
-        # because those are float numbers, we will round them, and count the unique values of the array
-        # (unique, counts) = np.unique(np.round(scaledYchannel), return_counts=True)
         Ychannel = cv2.normalize(Ychannel, None, 0, 255, cv2.NORM_MINMAX)
+        #because those are float numbers, we will round them
         Ychannel = np.ceil(Ychannel)  # floating point precision correction
         Ychannel = Ychannel.astype('uint8') # turn all number to int
+        #  count the unique values of the array
         (unique, counts) = np.unique(np.round(Ychannel), return_counts=True)
 
         newY = np.zeros(256)  # create new np.array of zeroes in size of 256 because of the values [0,255]
@@ -205,7 +146,7 @@ def Regular_histogram_Building(imgOrig: np.ndarray) -> (np.ndarray,int):
         return finalArr_beforeCumSum,2  # return the final array of regular histogram (2 if RGB)
 def Cumsum_histogram_Building(histArray: np.ndarray) -> (np.ndarray):
     """
-    :param histArray: this is the array we got from the Regular_histogram_Building function
+    :param histArray: the array we got from the Regular_histogram_Building function
     :return: array of the cumsum
     """
     sum_mat = np.zeros(256)  # create new np.array of zeroes in size of 256 because of the values [0,255]
@@ -214,7 +155,7 @@ def Cumsum_histogram_Building(histArray: np.ndarray) -> (np.ndarray):
         if i == 0:
             sum_mat[i] = value  # first element will be the same
         else:
-            sum_mat[i] = sum_mat[i - 1] + value
+            sum_mat[i] = sum_mat[i - 1] + value #sum all numbers
 
         i = i + 1  # increase the index
 
@@ -228,7 +169,7 @@ def LUT_Building(SumArr: np.ndarray) -> (np.ndarray):
     """
     LUTarr = np.zeros(256)  # create new np.array of zeroes in size of 256 because of the values [0,255]
     for i in range(len(SumArr)):
-        LUTarr[i]=math.ceil((SumArr[i]*255)/(SumArr[len(SumArr)-1])) #excactly the same formula as written in the PDF
+        LUTarr[i]=math.ceil((SumArr[i]*255)/(SumArr[len(SumArr)-1])) # exactly the same formula as written in the PDF
     return LUTarr
 
 def ApplyLUT(lutArr: np.ndarray,imOrig:np.ndarray) -> (np.ndarray):
@@ -255,7 +196,6 @@ def ApplyLUT(lutArr: np.ndarray,imOrig:np.ndarray) -> (np.ndarray):
         #after applying the LUT , we get values in Ychannel between 0-255
         imgEq[...,0]=imgEq[...,0]/255 #we will normalized y channel to values between 0-1
         imgEq=transformYIQ2RGB(imgEq) # we will return it to RGB image
-        plt.imshow(imgEq)
         return imgEq
 
 
@@ -266,15 +206,7 @@ def hsitogramEqualize(imOrig:np.ndarray)->(np.ndarray,np.ndarray,np.ndarray):
             :param imgOrig: Original Histogram
             :ret
         """
-    if len(imOrig.shape) == 2:
-        greyscale = True
-    elif len(imOrig.shape) == 3:
-        greyscale = False
-    else:
-        raise ValueError('Unsupported array representation. only RGB or Greyscale images allowed')
-    tempImg = np.copy(imOrig)
-
-    histOrig,ColorModel=Regular_histogram_Building(imOrig)    # Original Histogram
+    histOrig,ColorModel=Regular_histogram_Building(imOrig) # Original Histogram
     cumSum=Cumsum_histogram_Building(histOrig)  #CumSum
     # plt.title('Original image histogram with CDF') #display them
     # plt.plot(cumSum, color='b')
@@ -284,25 +216,12 @@ def hsitogramEqualize(imOrig:np.ndarray)->(np.ndarray,np.ndarray,np.ndarray):
     lutArr = LUT_Building(cumSum)
     imgAfterLUT=ApplyLUT(lutArr,imOrig) #image equlized
     histEq,ColorModel=Regular_histogram_Building(imgAfterLUT)    # Original Histogram
-    plt.subplot(2, 1, 2)
-    plt.title('Equalized image histogram with CDF ')
-    # plt.plot(cdf_normalized, color='b')
-    # plt.hist(imEq.flatten(), 256, [0, 255], color='r')
-    # plt.xlim([0, 255])
-    # plt.legend(('cdf - EQUALIZED', 'histogram - EQUALIZED'), loc='upper right')
-    # plt.show()
-
-    # if ColorModel==1: #1 if grayscale
-    #     pass
-    # else:# and 2 if RGB
-    #     pass
 
     # display the original image
     cv2.imshow('ORIGINAL image', imOrig)
 
     # display equalized image
     cv2.imshow('EQUALIZED image', imgAfterLUT)
-
 
     return imgAfterLUT, histOrig, histEq
 
@@ -314,10 +233,80 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :param nIter: Number of optimization loops
         :return: (List[qImage_i],List[error_i])
     """
-    if len(imOrig.shape) == 2:
-        greyscale = True
-    elif len(imOrig.shape) == 3:
-        greyscale = False
-    else:
-        raise ValueError('Unsupported array representation. only RGB or Greyscale images allowed')
+    if(len(imOrig.shape) != 3 and len(imOrig.shape) !=2):
+        raise ValueError('Unsupported array representation. only RGB or Grayscale images allowed')
+    if len(imOrig.shape) == 3:# If this is RGB (3D)
+        imYIQ = transformRGB2YIQ(imOrig)
+        imY = imYIQ[:, :, 0].copy()  # take only y channel
+    else:# If this is GrayScale (2D)
+        imY = imOrig
+    histOrig = np.histogram(imY.flatten(), bins=256)[0] # Original Histogram
+    Z, Q = findBestCenters(histOrig, nQuant, nIter)
+    imHistory = [imOrig.copy()]
+    E = []
+    for i in range(len(Z)):
+        arrayQuant = np.array([Q[i][k] for k in range(len(Q[i])) for x in range(Z[i][k], Z[i][k + 1])])
+        q_img, e = convertToImg(imY, histOrig, imYIQ if len(imOrig.shape) == 3 else [], arrayQuant)
+        imHistory.append(q_img)
+        E.append(e)
+    return imHistory, E
 
+def findBestCenters(histOrig: np.ndarray, nQuant: int, nIter: int) -> (np.ndarray, np.ndarray):
+    """
+            function find the best nQuant centers for quantize the image in nIter steps given us, *or* when the error is minimum
+            :param histOrig: hist of the original image
+            :param nQuant: Number of colors to quantize the image to
+            :param nIter: Number of optimization loops
+            :return: return all centers and their color selected to build from it all the images.
+        """
+    Q = []
+    Z = []
+    # head start, all the intervals are in the same length
+    z = np.arange(0, 256, round(256 / nQuant))
+    z = np.append(z, [255])
+    Z.append(z.copy())
+    q = fix_q(z, histOrig)
+    Q.append(q.copy())
+    for n in range(nIter):
+        z = fix_z(q)
+        if (Z[-1] == z).all():  #if nothing changed we will break
+            break
+        Z.append(z.copy())
+        q = fix_q(z, histOrig)
+        Q.append(q.copy())
+    return Z, Q
+
+def fix_z(q: np.array) -> np.array:
+    """
+        Calculate new z, using the formula from the lecture:
+        z[i]=q[i-1]-q[i]\2
+        :param q: the new list of q
+        :return: the new z- after fixing the boundary
+    """
+    z_new = np.array([round((q[i - 1] + q[i]) / 2) for i in range(1, len(q))]).astype(int) #we will find the middle
+    z_new = np.concatenate(([0], z_new, [255]))
+    return z_new
+
+def fix_q(z: np.array, image_hist: np.ndarray) -> np.ndarray:
+    """
+        Calculate new q using wighted average on the histogram
+        :param image_hist: the histogram of the original image
+        :param z: the new list of centers
+        :return: the new list of wighted average
+    """
+    q = [np.average(np.arange(z[k], z[k + 1] + 1), weights=image_hist[z[k]: z[k + 1] + 1]) for k in range(len(z) - 1)]
+    return np.round(q).astype(int)
+
+def convertToImg(imOrig: np.ndarray, histOrig: np.ndarray, imYIQ: np.ndarray, arrayQuantize: np.ndarray) -> (np.ndarray, float):
+    """
+        Executing the quantization to the original image
+        :return: returning the resulting image and the MSE.
+    """
+    imageQ = np.interp(imOrig, np.linspace(0, 1, 255), arrayQuantize)
+    curr_hist = np.histogram(imageQ, bins=256)[0]
+    err = np.sqrt(np.sum((histOrig.astype('float') - curr_hist.astype('float')) ** 2)) / float(
+        imOrig.shape[0] * imOrig.shape[1])
+    if len(imYIQ):  # if the original image is RGB
+        imYIQ[:, :, 0] = imageQ / 255
+        return transformYIQ2RGB(imYIQ), err
+    return imageQ, err
